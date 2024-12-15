608e8e05ce889da4e6df1a625db5cde5b0ec5bc4
daslu <daniel.slutsky@gmail.com>
2024-12-16 01:45:33 +0200

    version "1-beta5"

--- build.clj
+++ build.clj
@@ -15,7 +15,7 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'org.scicloj/tableplot)
-(def version "1-beta4")
+(def version "1-beta5")
 
 (def snapshot (str version "-SNAPSHOT"))
 (def class-dir "target/classes")
